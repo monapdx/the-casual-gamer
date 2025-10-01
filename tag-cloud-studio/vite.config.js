c363f9122186e6ab3ab0b83183e952fc9b60105c
monapdx <thegirlnextfloor@gmail.com>
2025-10-01 02:31:03 -0700

    N/A

--- tag-cloud-studio/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/tag-cloud-studio/',   // <-- use your repo name here
-})
